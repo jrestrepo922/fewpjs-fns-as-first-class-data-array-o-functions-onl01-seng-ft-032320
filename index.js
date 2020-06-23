7948305890fe4d9c6aa533d07c64b720c9d1ef85
JUAN M RESTREPO <tinto922@gmail.com>
2020-06-23 12:52:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function unleashDog(dogName, dogBreed) {
 }
 
 
-const routine = [wakeDog(), leashDog(), walkToPark(), throwFrisbee(), walkHome(), unleashDog()]
+const routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog]
 
 function exerciseDog(dogName, dogBreed){
   for (let i = 0; i < routine.length; i++){
