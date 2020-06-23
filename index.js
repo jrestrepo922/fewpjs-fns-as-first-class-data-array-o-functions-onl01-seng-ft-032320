f604b54a2c33fe08e8585ffa21b064e379c92cc0
JUAN M RESTREPO <tinto922@gmail.com>
2020-06-23 12:44:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function walkToPark(dogName, dogBreed) {
 
 function throwFrisbee(dogName, dogBreed) {
   console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`)
-  return `Wake ${dogName} the ${dogBreed}`
+  return `Throw the frisbee for ${dogName} the ${dogBreed}`
 }
 
 function walkHome(dogName, dogBreed) {
