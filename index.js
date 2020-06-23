9eafe6d798b7f7cee0adb8aaa6577d6e334c1a6d
JUAN M RESTREPO <tinto922@gmail.com>
2020-06-23 12:41:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,28 @@ function wakeDog(dogName, dogBreed) {
   console.log(`Wake ${dogName} the ${dogBreed}`)
   return `Wake ${dogName} the ${dogBreed}`
 }
+
+function leashDog(dogName, dogBreed) {
+  console.log(`Wake ${dogName} the ${dogBreed}`)
+  return `Wake ${dogName} the ${dogBreed}`
+}
+
+function walkToPark(dogName, dogBreed) {
+  console.log(`Wake ${dogName} the ${dogBreed}`)
+  return `Wake ${dogName} the ${dogBreed}`
+}
+
+function throwFrisbee(dogName, dogBreed) {
+  console.log(`Wake ${dogName} the ${dogBreed}`)
+  return `Wake ${dogName} the ${dogBreed}`
+}
+
+function wakeDog(dogName, dogBreed) {
+  console.log(`Wake ${dogName} the ${dogBreed}`)
+  return `Wake ${dogName} the ${dogBreed}`
+}
+
+function wakeDog(dogName, dogBreed) {
+  console.log(`Wake ${dogName} the ${dogBreed}`)
+  return `Wake ${dogName} the ${dogBreed}`
+}
